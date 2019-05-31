f5f9d559c9bba0a751421002ec5bfed427a198cb
yezw <ye19861102>
2019-05-31 10:51:26 +0800

    second  day

---
+++ config-server/src/main/java/com/ye/springcloud/ConfigServerApplication.java
@@ -0,0 +1,11 @@
+package com.ye.springcloud;
+
+import org.springframework.boot.SpringApplication;
+
+public class ConfigServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ConfigServerApplication.class, args);
+    }
+
+}
